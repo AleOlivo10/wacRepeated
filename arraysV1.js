9d73bd90e4f7b7ed0638ff5175011728ae5e49b4
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-11-14 22:54:06 +0000

    completed practice with v1

---
+++ arraysV1.js
@@ -0,0 +1,15 @@
+var exampleArray = ["item1", "item2", "item3"]
+
+//this is an array. it stores item, such as strings, integers, and so on.
+//to call on the items in the array on the repl line, enter the name of the array (after entering the enter line of code)
+//if you write exampleArray[1], you will get 'item2'. you can do this to reference a specific item. keep in mind that computers start counting at zero
+
+//TO ADD: if you add .push to the end of the variable, it adds(pushes!) and item to the end of the array
+//when you enter exampleArray.push("item4") onto the repl line, the console will give you a '4' to show you how many items there are in the array nowe
+
+// TO CHANGE: writing exampleArray[0] = 'item 1 updated' will change a specific item, in this case item 1. Keep in mind this is all happening on the repl line
+
+//TO DELETE: writing exampleArray.splice[0,1] will delete an item. the first number indicated where in the array to start deleting (positioning) and the 2nd number indicates how many items are being deleted.
+//when you  do this on the repl line, the terminal will tell you what the last item you deleted was
+
+//BASIC COMMANDS: .push to add, .splice to delete
