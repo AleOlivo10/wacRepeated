d3ee296f37e6331da259dd00aeb3a3c736e79411
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-11-15 22:42:05 +0000

    i'm mid lesson for this one

---
+++ forloopV5.js
@@ -0,0 +1,10 @@
+i =0                    //initialization
+say 'hey' if i < 3      //condition
+increase i by 1         //final-expression
+
+//this is written in pseudo code and its basically telling the computer to do what's below.
+ 
+0 "hey"
+1 "hey"
+2 "hey"
+3 
